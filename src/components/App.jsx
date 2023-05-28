708d631c2fc06eae866fae0f5ae81bee4aeea680
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2023-05-28 10:46:53 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,16 @@
-import React, { Component } from 'react';
-import { Statistics } from './Statistics/Statistics';
-import { Section } from './Section';
-import { FeedbackOptions } from './FeedbackOptions';
-import { Notification } from './Notification/Notification';
-
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  handleAddGood = () => {
-    this.setState(state => ({
-      good: state.good + 1,
-    }));
-  };
-  handleAddNeutral = () => {
-    this.setState(state => ({
-      neutral: state.neutral + 1,
-    }));
-  };
-  handleAddBad = () => {
-    this.setState(state => ({
-      bad: state.bad + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    const percentage = (this.state.good * 100) / total;
-    const number = Math.round(percentage) + '%';
-    if (number === 'NaN%') return 0 + '%';
-    return number;
-  };
-
-  render() {
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            good={this.handleAddGood}
-            neutral={this.handleAddNeutral}
-            bad={this.handleAddBad}
-          />
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-          <Notification
-            message="There is no feedback"
-            total={this.countTotalFeedback()}
-          />
-        </Section>
-      </div>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
